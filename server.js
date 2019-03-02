60f083eed48f6d938b5f9bdcd44eace853203877
Pradeesh Manayangath <pradeesh@tm7.tech>
2019-03-02 15:47:44 -0500

    Code updates

--- server.js
+++ server.js
@@ -1,30 +1,90 @@
+//EXpress Config
 var express = require('express'),
     app = express(),
     //port = process.env.PORT || 7000,
-    mongoose = require('mongoose'),
-    Item = require('./api/model/tfsitemmodel'),
-    User = require('./api/model/tfsusermodel'),//created model loading here
+    Datastore = require('nedb'),
+    // Item = require('./api/model/tfsitemmodel'),
+    // User = require('./api/model/tfsusermodel'),//created model loading here
     bodyParser = require('body-parser');
 
-	app.set('view engine', 'ejs')
+//DB config
+var User = new Datastore();
+var Item = new Datastore();
 
-// mongoose instance connection url connection
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://tfs:Turing%234321@cluster0-shard-00-00-omg4j.mongodb.net:27017,cluster0-shard-00-01-omg4j.mongodb.net:27017,cluster0-shard-00-02-omg4j.mongodb.net:27017/tfs?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true', {useNewUrlParser: true});
+var admin = {
+  username: 'admin',
+  password: 'admin'
+};
+
+//Initial Data setup
+User.insert(admin, function(err, user) {
+  console.log('Inserted', user.username, 'with ID', user._id);
+});
+
+//EJS
+app.set('view engine', 'ejs')
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-
-var routes = require('./api/route/tfsroute'); //importing route
-routes(app); //register the route
-
-
 //app.listen(port);
 app.listen(7000);
 
-app.use(function(req, res) {
-  res.status(404).send({url: req.originalUrl + ' not found'})
+// app.use(function(req, res) {
+//   res.status(404).send({url: req.originalUrl + ' not found'})
+// });
+
+//Services
+
+app.get('/', function(req, res) {
+  res.render('pages/login');
+});
+
+
+
+app.get('/login', function(req, res) {
+  console.log(req.query.username);
+  User.findOne({username:req.query.username,password:req.query.password}, function(err, user) {
+    console.log(user);
+    if (err || user == null) {
+      res.render('pages/login', {
+        message: "Invalid user name or password"
+      });
+    }
+    res.locals.user = user;
+    Item.find({}, function(err, items) {
+      if(err)
+        items = [];
+      res.render('pages/dashboard', {
+        items: items
+      });
+    });
+  });
+
+  app.get('/addItem', function(req, res) {
+    // res.locals.user = req.session.user;
+    res.render('pages/addItem');
+  });
+
+  app.post('/itemSubmit', function(req, res) {
+    console.log(req.locals);
+    // res.locals.user = req.session.user;
+    Item.insert(req.body, function(err, item) {
+      if (err || item == null) {
+        res.render('pages/addItem', {
+          message: "Adding item failed"
+        });
+      }
+      Item.find({}, function(err, items) {
+        if(err)
+          items = [];
+        res.render('pages/dashboard', {
+          items: items
+        });
+      });
+    });
+  });
+
 });
 
 
